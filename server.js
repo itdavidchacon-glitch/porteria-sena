17644298af450004bb51168f88ec4c5c1ec00b7b
Alejandro <alejogarcia0324@gmail.com>
2025-12-10 12:25:37 -0500

    Modificacion de css

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ let app = express()
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'src')))
+app.use('/css', express.static(path.join(__dirname, 'css')))
+app.use('/js', express.static(path.join(__dirname, 'js')))
+app.use('/acciones', express.static(path.join(__dirname, 'acciones')))
+app.use('/img', express.static(path.join(__dirname, 'img')))
 
 const puerto = 3000
 
 app.listen(3000, () => {
-    console.log(`Server is running on port 3000 http://localhost:${puerto} `);
-}
-);
+    console.log(`Server is running on port 3000 http://localhost:3000`);
+})
